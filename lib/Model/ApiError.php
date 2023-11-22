c43adcf5f34e0678980c346f1d502abb96f76d94
Diller Synthetics [Code Gen.] <synthetics@diller.io>
2023-11-22 17:47:16 +0000

    Update sdk-php, 2023-11-22

--- lib/Model/ApiError.php
+++ lib/Model/ApiError.php
@@ -2,7 +2,7 @@
 /**
  * ApiError
  *
- * PHP version 7.4 
+ * PHP version 7.4
  *
  * @category Class
  * @package  DillerAPI
--- lib/Model/Consent.php
+++ lib/Model/Consent.php
@@ -2,7 +2,7 @@
 /**
  * Consent
  *
- * PHP version 7.4 
+ * PHP version 7.4
  *
  * @category Class
  * @package  DillerAPI
